c70112e335b8cec12ac9ee802a34351f219ea936
Yousef Almutairi <72383597+code-l0n3ly@users.noreply.github.com>
2024-04-03 19:57:11 +0300

    Commit

--- main.go
+++ main.go
@@ -8,5 +8,5 @@ import (
 func main() {
 	app := App.App{}
 	app.Initialize("root", "wgLCfSQUYtKqCGBfviHSyMRtIloljyqm", "viaduct.proxy.rlwy.net:38199", "Hotel")
-	app.Run("0.0.0.0:3000")
+	app.Run("8080")
 }
